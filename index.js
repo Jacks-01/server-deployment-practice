5b9bb6b507ea332da8f6b09ca5b19efd7cf45ab3
Jack Stubblefield <jack011601@gmail.com>
2022-09-20 01:41:07 +0000

    added testing

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-'use strict'
+'use strict';
 
-const express = require('express');
+const { start } = require('./src/app');
 
-const app = express();
-const notFound = require('./error-handling/404');
-const errorHandler = require('./error-handling/500');
-const PORT = process.env.PORT || 3001
-
-
-app.get('/', (req, res, next) => {
-    res.status(200).send('Hello There')
-});
-
-app.get('/bad', (req, res, next) => {
-    next('this is a bad route');
-});
-
-app.use('*', notFound);
-
-
-app.use(errorHandler)
-
-app.listen(PORT, () => {
-    console.log('app is listening on port 3001')
-}); 
+start();
---
+++ src/__tests__/server.test.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const supertest = require('supertest');
+const { app } = require('../app');
+const request = supertest(app);
+    
